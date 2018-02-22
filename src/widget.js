958888a6deb1199ba5cd07838d4dd78a7f401264
Edgar <edgar@reva.tech>
2018-02-22 11:53:10 +0000

    Refactor

--- src/widget.js
+++ src/widget.js
@@ -1,14 +1,14 @@
 (function() {
-    if (typeof _bftn_options == "undefined") _bftn_options = {};
-    if (typeof _bftn_options.iframe_base_path == "undefined") _bftn_options.iframe_base_path = 'https://widget.battleforthenet.com/iframe';
-    if (typeof _bftn_options.animation == "undefined") _bftn_options.animation = 'main';
-    if (typeof _bftn_options.delay == "undefined") _bftn_options.delay = 1000;
-    if (typeof _bftn_options.debug == "undefined") _bftn_options.debug = false;
-    if (typeof _bftn_options.date == "undefined") _bftn_options.date = new Date(2017, 11 /* Zero-based month */, 14);
-    if (typeof _bftn_options.viewCookieExpires == "undefined") _bftn_options.viewCookieExpires = 1;
-    if (typeof _bftn_options.actionCookieExpires == "undefined") _bftn_options.actionCookieExpires = 7;
-    if (typeof _bftn_options.always_show_widget == "undefined") _bftn_options.always_show_widget = false;
-    if (typeof _bftn_options.theme == "undefined") _bftn_options.theme = 'capitol';
+    if (typeof _widget_options == "undefined") _widget_options = {};
+    if (typeof _widget_options.iframe_base_path == "undefined") _widget_options.iframe_base_path = 'http://widget.safetyauth.com/contact';
+    // if (typeof _widget_options.animation == "undefined") _widget_options.animation = 'main';
+    // if (typeof _widget_options.delay == "undefined") _widget_options.delay = 1000;
+    if (typeof _widget_options.debug == "undefined") _widget_options.debug = false;
+    // if (typeof _widget_options.date == "undefined") _widget_options.date = new Date(2017, 11 /* Zero-based month */, 14);
+    // if (typeof _widget_options.viewCookieExpires == "undefined") _widget_options.viewCookieExpires = 1;
+    // if (typeof _widget_options.actionCookieExpires == "undefined") _widget_options.actionCookieExpires = 7;
+    // if (typeof _widget_options.always_show_widget == "undefined") _widget_options.always_show_widget = false;
+    if (typeof _widget_options.theme == "undefined") _widget_options.theme = 'capitol';
   
     var _bftn_animations = {
       main: {
@@ -24,16 +24,16 @@
           return this;
         },
         start: function() {
-          var iframe = _bftn_util.createIframe();
-          _bftn_util.bindIframeCommunicator(document.getElementById('_bftn_iframe'), this);
+          var iframe = _widget_util.createIframe();
+          _widget_util.bindIframeCommunicator(document.getElementById('_bftn_iframe'), this);
         },
         stop: function() {
-          _bftn_util.destroyIframe();
+          _widget_util.destroyIframe();
         }
       }
     }
   
-    var _bftn_util = {
+    var _widget_util = {
       injectCSS: function(id, css) {
         var style = document.createElement('style');
         style.type = 'text/css';
@@ -48,7 +48,7 @@
         wrapper.id = '_bftn_wrapper';
         var iframe = document.createElement('iframe');
         iframe.id = '_bftn_iframe';
-        iframe.src = _bftn_options.iframe_base_path + '/iframe.html';
+        iframe.src = _widget_options.iframe_base_path + '/iframe.html';
         iframe.frameBorder = 0;
         iframe.allowTransparency = true; 
         iframe.style.display = 'none';
@@ -88,7 +88,7 @@
               animation.stop();
               break;
             case 'cookie':
-              _bftn_util.setCookie(e.data.name, e.data.val, e.data.expires);
+              _widget_util.setCookie(e.data.name, e.data.val, e.data.expires);
               break;
           }
         }, false);
@@ -118,7 +118,7 @@
       },
   
       log: function() {
-        if (_bftn_options.debug) console.log.apply(console, arguments);
+        if (_widget_options.debug) console.log.apply(console, arguments);
       }
     }
   
@@ -128,21 +128,21 @@
   
       function init() {
         setTimeout(function() {
-          _bftn_animations[_bftn_options.animation].init(_bftn_options).start();
-        }, _bftn_options.delay);
+          _bftn_animations[_widget_options.animation].init(_widget_options).start();
+        }, _widget_options.delay);
       }
   
       function preload() {
         for (i = 0; i < preload.arguments.length; i++) {
           images[i] = new Image()
-          images[i].src = _bftn_options.iframe_base_path + '/images/' + preload.arguments[i]
+          images[i].src = _widget_options.iframe_base_path + '/images/' + preload.arguments[i]
   
           images[i].onload = function() {
             preloaded++;
-            _bftn_util.log('Preloaded ' + preloaded + ' images.');
+            _widget_util.log('Preloaded ' + preloaded + ' images.');
   
             if (preloaded == images.length) {
-              _bftn_util.log('DONE PRELOADING IMAGES. Starting animation in ' + _bftn_options.delay + ' milliseconds.');
+              _widget_util.log('DONE PRELOADING IMAGES. Starting animation in ' + _widget_options.delay + ' milliseconds.');
               init();
             }
           }
@@ -150,24 +150,22 @@
       }
   
       // Should we show the widget, regardless?
-      if (!_bftn_options.always_show_widget && window.location.href.indexOf('ALWAYS_SHOW_BFTN_WIDGET') === -1) {
+      // if (!_widget_options.always_show_widget && window.location.href.indexOf('ALWAYS_SHOW_BFTN_WIDGET') === -1) {
   
-        // Don't show widget if cookie has been set.
-        if (
-          _bftn_util.getCookie('_BFTN_WIDGET_VIEW') ||
-          _bftn_util.getCookie('_BFTN_WIDGET_ACTION')
-        ) {
-          return;
-        }
-      }
+      //   // Don't show widget if cookie has been set.
+      //   if (
+      //     _widget_util.getCookie('_BFTN_WIDGET_VIEW') ||
+      //     _widget_util.getCookie('_BFTN_WIDGET_ACTION')
+      //   ) {
+      //     return;
+      //   }
+      // }
   
       // Only show once per day.
-      _bftn_util.setCookie('_BFTN_WIDGET_VIEW', 'true', _bftn_options.viewCookieExpires);
+      // _widget_util.setCookie('_BFTN_WIDGET_VIEW', 'true', _widget_options.viewCookieExpires);
   
-      _bftn_util.injectCSS('_bftn_iframe_css', '#_bftn_wrapper { position: fixed; left: 0px; top: 0px; width: 100%; height: 100%; z-index: 20000; -webkit-overflow-scrolling: touch; overflow-y: auto; } #_bftn_iframe { width: 100%; height: 100%;  }');
+      // _widget_util.injectCSS('_bftn_iframe_css', '#_bftn_wrapper { position: fixed; left: 0px; top: 0px; width: 100%; height: 100%; z-index: 20000; -webkit-overflow-scrolling: touch; overflow-y: auto; } #_bftn_iframe { width: 100%; height: 100%;  }');
   
-      // Preload images before showing the animation
-      // preload();
       init();
     }
   
